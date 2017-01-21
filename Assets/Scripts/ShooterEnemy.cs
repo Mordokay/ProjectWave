3e19d9217e09cdf67fcffe3724b48c8da1e8aded
Mordokay <papajorgioster@gmail.com>
2017-01-21 17:53:19 +0000

    Spawner half working and stuff

--- Assets/Scripts/ShooterEnemy.cs
+++ Assets/Scripts/ShooterEnemy.cs
@@ -12,23 +12,37 @@ public class ShooterEnemy : MonoBehaviour {
     public Transform bulletSpawnPoint;
     public float bulletSpeed;
 
+    public bool onSight;
+
     // Use this for initialization
     void Start () {
-        InvokeRepeating("LaunchProjectile", 0.0f, 1.0f);
+        onSight = false;
+        InvokeRepeating("LaunchProjectile", 0.0f, 5.0f);
         target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
 	// Update is called once per frame
 	void Update () {
         range = Vector2.Distance(this.transform.position, target.position);
-        if (Mathf.Abs(range-distance) > 0.05f)
+
+        if (Mathf.Abs(range - distance) > 0.05f && Mathf.Abs(range - distance) < 6.0f)
         {
+            onSight = true;
+
             transform.position = Vector2.MoveTowards(transform.position, (transform.position - target.position).normalized * distance + target.position, speed * Time.deltaTime);
+
+            Vector3 diff = target.position - transform.position;
+            diff.Normalize();
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, rot_z - 90.0f);
         }
-        Vector3 diff = target.position - transform.position;
-        diff.Normalize();
-        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rot_z - 90.0f);
+        //resets onsight and gives the enemy a velocity AWAY from the player
+        else if (onSight)
+        {
+            onSight = false;
+            Debug.Log(-(target.transform.position - transform.position).normalized * speed);
+            this.GetComponent<Rigidbody2D>().velocity = -(target.transform.position - transform.position).normalized * speed;
+        }
     }
 
     void LaunchProjectile()
@@ -37,6 +51,7 @@ public class ShooterEnemy : MonoBehaviour {
         {
             //TODO Shuold only launch when its close to the player
             GameObject myBullet = Instantiate(bullet);
+            Destroy(myBullet, 5.0f);
             myBullet.transform.position = bulletSpawnPoint.position;
             myBullet.GetComponent<Rigidbody2D>().velocity = (target.position - transform.position).normalized * bulletSpeed;
         }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,6 +5,36 @@ using UnityEngine;
 [System.Serializable]
 public class Spawner : MonoBehaviour {
 
+    float timeToNextSpawn;
+    float elapsedTime;
+    float spawnDecreaseTimeRatio;
+    public List<spawnPoint> mySpawns;
+
+    public List<GameObject> mySpawnables;
+
+    public float speedMinVal;
+    public float speedMaxVal;
+
+    void Start()
+    {
+        elapsedTime = 999.0f;
+        timeToNextSpawn = 0.5f;
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > timeToNextSpawn)
+        {
+            GameObject myEnemy = Instantiate(mySpawnables[Random.Range(0, mySpawnables.Count)]) as GameObject;
+            spawnPoint myPoint = mySpawns[Random.Range(0, mySpawns.Count)];
+            myEnemy.transform.position = myPoint.originPoint.position;
+            myEnemy.GetComponent<Rigidbody2D>().velocity = (myPoint.directionPoint.position - myPoint.originPoint.position).normalized * Random.Range(speedMinVal, speedMaxVal);
+            elapsedTime = 0.0f;
+        }
+    }
+
     [System.Serializable]
     public class spawnPoint
     {
@@ -12,14 +42,9 @@ public class Spawner : MonoBehaviour {
         public Transform originPoint;
         public Transform directionPoint;
     }
-
-    public List<spawnPoint> mySpawns;
-
-    void Start () {
-        mySpawns = new List<spawnPoint>();
-    }
 	
-	void Update () {
-		
-	}
+    void Spawn()
+    {
+
+    }
 }
