fe387902747d4d2718f388430503318c0f22ee3c
Mordokay <papajorgioster@gmail.com>
2017-01-21 11:24:48 +0000

    Small fixes

--- Assets/Scripts/PlayerRange.cs
+++ Assets/Scripts/PlayerRange.cs
@@ -2,91 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerRange : MonoBehaviour {
-
-    private bool isPressing;
-    private bool toFire;
+public class PlayerRange : MonoBehaviour
+{
 
     public Transform RangeCenter;
-    
 
-    public GameObject Ammo;
+    private bool isPressing;
 
-	// Use this for initialization
-	void Start () {
+    private GameObject Ammo;
+    public GameObject distortion;
 
-        GetComponent<PolygonCollider2D>().enabled = false;
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().enabled = false;
+        distortion.SetActive(false);
         isPressing = false;
-        toFire = false;
         Ammo = null;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetMouseButton(0))
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
         {
             isPressing = true;
-            GetComponent<PolygonCollider2D>().enabled = true;
+            this.GetComponent<SpriteRenderer>().enabled = true;
+            distortion.SetActive(true);
         }
         else
         {
+            distortion.SetActive(false);
+            this.GetComponent<SpriteRenderer>().enabled = false;
             isPressing = false;
-            GetComponent<PolygonCollider2D>().enabled = false;
-
-            if (toFire && Ammo)
+            if(Ammo != null)
             {
-                
+                Ammo.GetComponent<Expelir>().isFired = true;
                 Vector2 vec = Ammo.transform.position - gameObject.transform.position;
-                Ammo.GetComponent<Rigidbody2D>().AddForce(-vec * Random.Range(1000, 1500));
-                toFire = false;
+                Ammo.GetComponent<Rigidbody2D>().AddForce(vec * 10000);
                 Ammo = null;
             }
             
-
-        }
-
-	}
-    
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Expelir" && Ammo == null)
-        {
-            Ammo = collision.gameObject;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Expelir" && Ammo != null)
-        {
-            Ammo = null;
-        }
-    }
+
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.tag == "Items" && isPressing)
+        if (collision.tag == "Items" && isPressing)
         {
-            if(Vector2.Distance(collision.gameObject.transform.position, RangeCenter.position) <= 0.8f)
+            
+            if (Vector2.Distance(collision.gameObject.transform.position, gameObject.transform.position) <= 1.0f)
             {
                 Destroy(collision.gameObject);
             }
-            collision.transform.position = Vector2.Lerp(collision.transform.position, RangeCenter.position, 6 * Time.deltaTime);
+            collision.transform.position = Vector2.Lerp(collision.transform.position, gameObject.transform.position, 6 * Time.deltaTime);
         }
 
-        if (Ammo != null && isPressing)
+        if(collision.tag == "Expelir" && isPressing)
         {
-            if (Vector2.Distance(Ammo.transform.position, RangeCenter.position) <= 0.8f)
-            {
+           if(Ammo == null)
+           {
+                if (Vector2.Distance(collision.transform.position, gameObject.transform.position) <= 0.4f)
+                {
+                    collision.transform.position = RangeCenter.position;
+                    Ammo = collision.gameObject;
+                }
+                collision.transform.position = Vector2.Lerp(collision.transform.position, gameObject.transform.position, 6 * Time.deltaTime);
+           }
+           else
+           {
                 Ammo.transform.position = RangeCenter.position;
-                toFire = true;
-            }
-            else
-            {
-                Ammo.transform.position = Vector2.Lerp(Ammo.transform.position, RangeCenter.position, 6 * Time.deltaTime);
-            }
-            
+           }
+
         }
-        
     }
+
+
 }
