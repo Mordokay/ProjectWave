a47b2a350c07a0fe426c14ed4b9709994e79d2a0
Mordokay <papajorgioster@gmail.com>
2017-01-21 16:13:10 +0000

    Spawner template script

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spawner : MonoBehaviour {
+
+    [System.Serializable]
+    public class spawnPoint
+    {
+
+        public Transform originPoint;
+        public Transform directionPoint;
+    }
+
+    public List<spawnPoint> mySpawns;
+
+    void Start () {
+        mySpawns = new List<spawnPoint>();
+    }
+	
+	void Update () {
+		
+	}
+}
