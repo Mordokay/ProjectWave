69c5d326d7938cfa1c0114a1bea6c5ec5cdb4670
ricardo pereira <ricardo pereira>
2017-01-21 13:04:55 +0000

    mine wandering off

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,10 +8,12 @@ public class EnemyMovement : MonoBehaviour {
     public float speed;
     public float minDistance;
     private float range;
+    private Vector3 direction;
 
     // Use this for initialization
     void Start () {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        InvokeRepeating("Wander", 0.0f, 5.0f);
     }
 
     // Update is called once per frame
@@ -22,6 +24,22 @@ public class EnemyMovement : MonoBehaviour {
         if (range < minDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+    }
+
+    void Wander()
+    {
+        range = Vector2.Distance(this.transform.position, target.position);
+
+        if (range > minDistance)
+        {
+            float randomX = Random.Range(-2.0f, 2.0f); // with float parameters, a random float
+            float randomY = Random.Range(-2.0f, 2.0f); //  between -2.0 and 2.0 is returned
+            direction = new Vector3(randomX, randomY,0.0f);
         }
     }
 }
