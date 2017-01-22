c8bc0fe13551d88030a8716d8e6fa90f471403fe
Mordokay <papajorgioster@gmail.com>
2017-01-22 14:24:00 +0000

    Alpha final

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
 
+    public Sprite mutedButton;
+    public Sprite unmutedButton;
+    public bool muted;
+
+    public Button Mutebutton;
+
     void Start()
     {
+        if (PlayerPrefs.GetInt("muted") == 1)
+        {
+            Mutebutton.image.overrideSprite = mutedButton;
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            Mutebutton.image.overrideSprite = unmutedButton;
+            muted = false;
+            AudioListener.pause = false;
+        }
         Time.timeScale = 1.0f;
     }
 
@@ -25,4 +44,21 @@ public class UIController : MonoBehaviour {
         Application.Quit();
     }
 
+    public void Mute()
+    {
+        if (muted)
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            muted = false;
+            AudioListener.pause = false;
+            Mutebutton.image.overrideSprite = unmutedButton;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("muted", 1);
+            muted = true;
+            AudioListener.pause = true;
+            Mutebutton.image.overrideSprite = mutedButton;
+        }
+    }
 }
---
+++ Assets/Scripts/menuSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuSpawner : MonoBehaviour {
+
+    public List<Sprite> mySprites;
+    public float elapsedTime;
+    public float timeToNextSpawn;
+
+    public List<spawnPoint> mySpawns;
+
+    void Start()
+    {
+        elapsedTime = 0.0f;
+    }
+
+    void Update () {
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime > timeToNextSpawn)
+        {
+            spawnPoint myPoint = mySpawns[Random.Range(0, mySpawns.Count)];
+
+            GameObject myObject = new GameObject();
+            myObject.transform.position = myPoint.originPoint.position;
+            myObject.AddComponent<Rigidbody2D>();
+            myObject.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+            myObject.GetComponent<Rigidbody2D>().velocity = (myPoint.directionPoint.position - myPoint.originPoint.position).normalized * 5.0f;
+            myObject.AddComponent<SpriteRenderer>();
+            myObject.GetComponent<SpriteRenderer>().sprite = mySprites[Random.Range(0, mySprites.Count)];
+
+            elapsedTime = 0.0f;
+
+            myObject.transform.localScale = new Vector3(7.0f, 7.0f, 7.0f);
+            Destroy(myObject, 8.0f);
+        }
+    }
+
+    [System.Serializable]
+    public class spawnPoint
+    {
+
+        public Transform originPoint;
+        public Transform directionPoint;
+    }
+}
